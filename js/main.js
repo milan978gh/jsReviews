5eb592df6592ba9e8ab1413467b00d5fc5c35298
milan978gh <milan978@gmail.com>
2020-10-18 15:37:01 +0200

    added images and started js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// local reviews data
+const reviews=[
+    {
+        id:1,
+        name:'milan antonic',
+        job:'web developer',
+        img:'url="../img/img1.jpg"',
+        text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, alias velit. Quae est voluptatibus iste sed nulla similique, consequatur quo.'
+    },
+    {
+        id:2,
+        name:'susan smith',
+        job:'UX developer',
+        img:'url="../img/img2.jpg"',
+        text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, alias velit. Quae est voluptatibus iste sed nulla similique, consequatur quo.'
+    },
+    {
+        id:3,
+        name:'anna jones',
+        job:'UI developer',
+        img:'url="../img/img3.jpg"',
+        text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, alias velit. Quae est voluptatibus iste sed nulla similique, consequatur quo.'
+    },
+    {
+        id:4,
+        name:'mary white',
+        job:'server-side developer',
+        img:'url="../img/img4.jpg"',
+        text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, alias velit. Quae est voluptatibus iste sed nulla similique, consequatur quo.'
+    }
+];
