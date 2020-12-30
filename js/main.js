f7957a687a9153fa7149d7935abe433b3446ba72
milan978gh <amilandev78@gmail.com>
2020-12-30 15:34:27 +0100

    js update

--- js/main.js
+++ js/main.js
@@ -29,5 +29,44 @@ const reviews = [
     text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, alias velit. Quae est voluptatibus iste sed nulla similique, consequatur quo.'
   }
 ];
+const personImg = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+// starting item
+let currentItem = 0;
 
 
+window.addEventListener('DOMContentLoaded', () => {
+  nextPerson();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  nextPerson();
+});
+nextBtn.addEventListener('click', () => {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  nextPerson();
+});
+randomBtn.addEventListener('click', () => {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  nextPerson();
+});
+
+
+function nextPerson() {
+  personImg.src = reviews[currentItem].img;
+  author.textContent = reviews[currentItem].name;
+  job.textContent = reviews[currentItem].job;
+  info.textContent = reviews[currentItem].text;
+}
